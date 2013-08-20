60b9746e1813e0a75a5d875a84c2622e11ed1fd7
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2013-08-20 12:15:56 +1000

    Add remote indicator, closes #3

--- bespoke-remote/remote.js
+++ bespoke-remote/remote.js
@@ -1,15 +1,20 @@
 +function(bespoke, window, document, undefined) {
   bespoke.plugins.remote = function(deck, options) {
     var options = {} || options,
-        socket = io.connect(options.socketUrl || 'http://localhost:<%= port %>/');
+        socket = io.connect(options.socketUrl || 'http://localhost:<%= port %>/'),
+        indicator = document.createElement('div');
 
+    indicator.className = 'bespoke-remote-indicator';
+    deck.parent.appendChild(indicator);
 
     socket.on('bespoke-remote.established', function() {
       console.info('Connection with server established.')
       console.info('Awaiting paired remote control...')
+      indicator.classList.add('bespoke-remote-indicator-established');
     })
     socket.on('bespoke-remote.connected', function() {
       console.info('Paired with remote control. Happy RCing!')
+      indicator.classList.add('bespoke-remote-indicator-connected');
     })
 
     socket.on('bespoke-remote.next', deck.next);
